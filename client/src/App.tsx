28a3ae6fd04088ddd1bed084716d072b2c5547fb
Aniket Kangane <115465575+Aniket897@users.noreply.github.com>
2024-07-17 16:47:49 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
 import EditorPage from "@/pages/EditorPage";
@@ -5,6 +6,18 @@ import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const App = () => {
+
+  useEffect(() => {
+    upInstance();
+  } , [])
+
+  const upInstance = async () => {
+    let response  = await fetch("https://codelab-th59.onrender.com/health");
+    response = await response.JSON();
+    console.log(response);
+  }
+
+  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Toaster richColors position="top-right" />
